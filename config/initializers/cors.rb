df17f3395323d9147385c19f3031e1d994d5d80c
Luis Osnet <luisosnet@gmail.com>
2023-02-22 12:34:32 -0600

    allow any directoy to host

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3001', /https*:\/\/.*?herokuapp\.com(?::\d{1,5})?)/, /https*:\/\/.*?ngrok\.io/
+    origins 'localhost:3001', /https*:\/\/.*?herokuapp\.com(\/.*)?$/, /https*:\/\/.*?ngrok\.io(\/.*)?$/
     resource '*', headers: :any, methods: [:get, :post, :put, :delete, :destroy, :options], expose: ['Authorization']
   end
 end
